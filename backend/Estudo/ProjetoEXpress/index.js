dc4f203602aa474e0dd8e87bd043297463aa2f9b
= <=>
2023-09-24 04:57:19 -0300

    extensão

---
+++ backend/Estudo/ProjetoEXpress/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+
+
+
+app.listen(8081, function(){
+    console.log("O servidor estar rodando da url http://localhost:8081")
+});
