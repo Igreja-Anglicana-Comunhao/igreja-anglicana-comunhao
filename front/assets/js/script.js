e2b8646a1f0a2520525c2a85fa19c7332e523724
IAbrahanArley <abrahanarley96@gmail.com>
2023-11-23 17:35:52 -0300

    footer

---
+++ front/assets/js/script.js
@@ -0,0 +1,64 @@
+const url = 'http://localhost:3001';
+function obterMensagens() {
+
+    var retorno = [];
+
+    var consulta = $.ajax({
+        url: { url } + '/cadastro',
+        method: 'GET',
+        dataType: 'json',
+        async: false
+    }).fail(function () {
+        return retorno;
+    });
+
+    consulta.done(function (data) {
+        retorno = data;
+    });
+
+    return retorno;
+}
+
+function inserirMensagem(obj) {
+
+
+
+    $.ajax({
+
+        url: { url } + '/cadastro',
+        type: 'POST',
+        data: JSON.stringify(obj),
+        contentType: 'application/json',
+        success: function(res){
+            console.log(res);
+        }
+    });
+}
+
+function validarUsuario(objLoginSenha) {
+
+
+    var retorno = false;
+
+    console.log(objLoginSenha);
+
+    var validacao = $.ajax({
+        url: { url } + '/users',
+        method: 'POST',
+        dataType: 'json',
+        async: false,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        contentType: 'application/json',
+        data: JSON.stringify(objLoginSenha)
+    }).fail(function () {
+        return retorno;
+    });
+
+    validacao.done(function (data) {
+        retorno = data;
+    });
+
+    return retorno;
+}
